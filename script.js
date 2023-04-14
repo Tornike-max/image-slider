77b119a00d6317b981a90fdc3e99ea0fbfb9db06
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2023-04-14 04:16:47 +0400

    create dots

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sliderDiv = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const btnRight = document.querySelector('.slider__btn--right');
 const btnLeft = document.querySelector('.slider__btn--left');
+const dotsContainer = document.querySelector('.dots')
 
 let currentSlide = 0;
 let maxSlide = slides.length;
@@ -11,19 +12,39 @@ const imgPositions = function(slide){
 }
 imgPositions(0)
 
+const dotFunc = function(){
+    slides.forEach((_,i)=> {
+       dotsContainer.insertAdjacentHTML('beforeend', `<button class="dots_dot" data-slide="${i}"></button>`)
+    })
+}
+dotFunc(slides)
+
+const activateDot = function(slide){
+    document.querySelectorAll('.dots_dot').forEach((dot)=>{
+        dot.classList.remove('dots-active')
+    });
+
+    document.querySelectorAll(`.dots_dot[data-slide="${slide}"]`).forEach(dot=>{
+        dot.classList.add('dots-active')
+    })
+    
+}
 
 
-const nextSlide = function(e){
+
+
+function nextSlide(e) {
     e.preventDefault();
- 
-    if(currentSlide === maxSlide - 1){
-     currentSlide = 0;
-    }else{
-     currentSlide++
+
+    if (currentSlide === maxSlide - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
     }
- 
-    imgPositions(currentSlide)
- }
+
+    imgPositions(currentSlide);
+    activateDot(currentSlide);
+}
 
  const prevSlide = function(e){
     e.preventDefault();
@@ -34,7 +55,16 @@ const nextSlide = function(e){
     }
 
     imgPositions(currentSlide)
+    activateDot(currentSlide)
 }
 
 btnRight.addEventListener('click',nextSlide)
 btnLeft.addEventListener('click',prevSlide)
+
+dotsContainer.addEventListener('click', function(e){
+   if(e.target.classList.contains('dots_dot')){
+    const{slide} = e.target.dataset
+    imgPositions(slide);
+    activateDot(slide)
+   }
+})
